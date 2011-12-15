9132917d344c7c2d8cf5d6f44299619ad2048c3e
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2011-12-15 16:20:05 +0100

    Documentation parser is now able to handle '@fields'

--- plugins/org.eclipse.koneki.ldt.parser/scripts/ldd.lua
+++ plugins/org.eclipse.koneki.ldt.parser/scripts/ldd.lua
@@ -27,52 +27,35 @@ end
 local value = function(node)
 	return node[1]
 end
-local findIdentifier= function(identifierName)
-	return function (lx)
-		repeat tk = lx:next()
-		until value(tk) == identifierName or tk.tag == 'Eof'
-		return tk ~='Eof'
-	end
-end
-local findAt = function (lx)
-	local tk
-	repeat
-		tk = lx:next()
-	until lx:is_keyword(tk, '@') or tk.tag == 'Eof'
-	return tk.tag~='Eof'
-end
-local passTaggedResult = function (lx)
-	local tagged = {}
-	for _, result in ipairs(lx) do
-		if type(result) == 'table' and result.tag then
-			table.insert(tagged, result)
-		end
-	end
-	return #tagged > 0 and tagged
-end
 local function findmaintag(nlx)
 	local parser = gg.sequence{
 		name = 'Main Tag Parser',
 		builder = function (x)
 			local res = {}
-			if #x == 1 then
+			local parsed = value(x)
+			if #parsed == 0 then
 				-- It is a file
-				res.kind = 'file'
+				res.tag = 'TFile'
+			elseif #parsed == 1 then
+				-- It's a record
+				res.tag = 'TRecordtype'
+				res.name = value(value(parsed))
 			end
 			return res
 		end,
 		'@@',
 		gg.multisequence{
 			gg.sequence{ 'file' },
-			gg.sequence{'recordtype', '[', '#', mlp.id, ']'},
+			gg.sequence{'recordtype', '#', mlp.id },
 			gg.sequence{'function', '[', gg.optkeyword('#'), mlp.id, ']', mlp.id}
 		}
 	}
-		nlx:add({"@@","file", "function", "recordtype"})
+		nlx:add({"@@","file", "function", "recordtype", "field"})
 	local result = parser(nlx)
 	return {
 		kind = result.kind,
-		tag  = result.kind:sub(1,1):upper().. result.kind:sub(2):lower()
+		name = result.name,
+		tag  = result.tag
 	}
 end
 local reference = gg.sequence({
@@ -113,27 +96,85 @@ local returnBlock = gg.sequence({
 	name = 'Return Documentation Statement Parser',
 	'@', 'return', referencesList
 })
+local composetyperef = function(vars)
+	local sequenceend
+	local ref = { tag = 'TTyperef'}
+	-- Deal with type less type reference
+	if #vars == 0 then
+		ref.module = ""
+		ref.type = ""
+		return ref, -1
+	elseif #vars > 0 then
+		-- Store type name
+		local last = vars[#vars]
+		ref.type = value( last )
+		sequenceend = last.lineinfo.last.offset
+	end
+	-- Compose module name
+	local tmp = {}
+	local id = 1
+	-- Storing names in an array with gaps
+	for index = 1, (#vars-1) * 2 , 2 do
+		tmp[index] = value(vars[id])
+		id = id + 1
+	end
+	-- Inserting dots in gaps
+	for index = 2, (#vars-2) * 2, 2 do
+		tmp[index] = '.'
+	end
+	-- Retrieve full module name
+	ref.module = table.concat(tmp)
+	return ref , sequenceend + 1
+end
 local function findreturntag(nlx)
 	local result = returnBlock(nlx)
 	local comment = {}
-	local sequenceend
+	local name, sequenceend
+	if #result > 0 then
+		comment.types = {tag = 'Types'}
+	end
 	for k = 1, #result do
-		local vars = result[k]
-		local tmp = {}
-		for id = 1, #vars do
-			if id == #vars then
-				table.insert(tmp, '#')
-				sequenceend = vars[id].lineinfo.last.offset
-			end
-			table.insert(tmp, vars[id][1])
-			if id < #vars - 1 then
-				table.insert(tmp, '.')
-			end
-		end
-		if not comment.types then comment.types = {tag = 'Types'} end
-		table.insert(comment.types, table.concat(tmp))
+		name, sequenceend = composetyperef(result[k])
+		table.insert(comment.types, name)
 	end
 	return comment, sequenceend
+end
+local completefieldblock = gg.sequence({
+	builder = function (x)
+		local fieldtype = composetyperef(value(x))
+		local nameend  = x[2].lineinfo.last.offset
+		return {
+			descriptionstart = nameend +1,
+			name = value(x[2]),
+			type = fieldtype
+		}
+	end,
+	'@','field', reference, mlp.id
+})
+local fieldblock = gg.sequence({
+	builder = function (x)
+		local token = value(x)
+		local name = value(token)
+		return {
+			descriptionstart = token.lineinfo.last.offset +1,
+			name =name,
+		}
+	end,
+	'@','field', mlp.id
+})
+local function findfieldtag(nlx)
+	local x = fieldblock(nlx)
+	local field = {	name = x.name }
+	return field, x.descriptionstart
+end
+local function findtypedfieldtag(nlx)
+	local x = completefieldblock(nlx)
+	local field = {
+		name = x.name, 
+		type = x.type
+	}
+--		table.print(field,1) 
+	return field, x.descriptionstart
 end
 function M.parse(string)
 	string = trim(string)
@@ -159,6 +200,15 @@ function M.parse(string)
 	if not at then
 		return comment
 	end
+	-- Cretate lexer used for parsing
+	local lx = lexer.lexer:clone()
+	lx.extractors = {
+		--	"extract_long_comment", "extract_short_comment",
+		--	"extract_long_string",
+		"extract_short_string", "extract_word", "extract_number",
+		"extract_symbol"
+	}
+	lx:add({"@@","file", "function", "recordtype", "return", "field"})
 	-- The rest of the comment should be special tags starting with '@'
 	while at <= #string do
 		-- Extract section to analyse
@@ -167,35 +217,47 @@ function M.parse(string)
 		if stringtoparse:len() > 0 then
 			-- Moving forward to next section
 			at = newline + 1
---			print (stringtoparse) 
-			-- Cretate lexer used for parsing
-			local lx = lexer.lexer:clone()
-			lx.extractors = {
-				--	"extract_long_comment", "extract_short_comment",
-				--	"extract_long_string",
-				"extract_short_string", "extract_word", "extract_number",
-				"extract_symbol"
-			}
-			lx:add({"@@","file", "function", "recordtype", "return"})
 			-- Detect main tags (file, function, recordtype)
 			if stringtoparse:find('@@') then
 				-- Parse tag
 				local maintag = findmaintag(lx:newstream(stringtoparse, 'Main tags lexer'))
 				if maintag then
 					-- Append tag information to collection
-					if not comment.maintag then comment.maintag = {} end
-					table.insert(comment.maintag, maintag)
+					if not comment.maintags then comment.maintags = {} end
+					table.insert(comment.maintags, maintag)
 				end
-			else
-				local valid, returns, offset = pcall(findreturntag, lx:newstream(stringtoparse, 'Return tags lexer'))
-				if valid then
+			elseif stringtoparse:find('@return')then
+				-- Search for a field
+				local returntag, returns, offset = pcall(findreturntag, lx:newstream(stringtoparse, 'Return tags lexer'))
+				if not returntag then
+					returntag, returns, offset = pcall(findreturntag, lx:newstream(stringtoparse, 'Return tags lexer'))
+				end
+				if returntag then
 					if not comment.returns then comment.returns = {} end
 					local returndesc ={
 						description = trim( stringtoparse:sub(offset + 1) ),
 						types = returns.types,
-						tag = 'Returns'
+						tag = 'TReturn'
 					}
 					table.insert(comment.returns, returndesc)
+				end
+			elseif stringtoparse:find('@field')then
+				local field, descriptionstart, x, valid = {}
+				if stringtoparse:find('#') then
+					valid,x, descriptionstart = pcall(findtypedfieldtag,lx:newstream(stringtoparse, 'Field tags lexer'))
+					if valid then
+						field.type = x.type
+					end
+				else
+					valid, x, descriptionstart = pcall(findfieldtag, lx:newstream(stringtoparse, 'Field tags lexer'))
+					if not valid then x = false end
+				end
+				if x then
+					field.name = x.name
+					field.description = trim(stringtoparse:sub(descriptionstart))
+					field.tag = 'TField'
+					if not comment.fields then comment.fields = {} end
+					table.insert(comment.fields, field)
 				end
 			end
 		end
