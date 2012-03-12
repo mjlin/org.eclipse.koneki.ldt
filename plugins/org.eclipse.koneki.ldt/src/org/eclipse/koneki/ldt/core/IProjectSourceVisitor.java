f861271781a8dcf4463d00ea747f0dc5c6b41ac4
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2012-03-12 16:47:56 +0100

    Implementing error handling support for project files visitor.

--- plugins/org.eclipse.koneki.ldt/src/org/eclipse/koneki/ldt/core/IProjectSourceVisitor.java
+++ plugins/org.eclipse.koneki.ldt/src/org/eclipse/koneki/ldt/core/IProjectSourceVisitor.java
@@ -10,12 +10,14 @@
  *******************************************************************************/
 package org.eclipse.koneki.ldt.core;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public interface IProjectSourceVisitor {
-	public abstract void processFile(final IPath absolutePath, final IPath relativePath, final String charset, final IProgressMonitor monitor);
+	public abstract void processFile(final IPath absolutePath, final IPath relativePath, final String charset, final IProgressMonitor monitor)
+			throws CoreException;
 
-	public abstract void processDirectory(final IPath absolutePath, final IPath relativePath, final IProgressMonitor monitor);
+	public abstract void processDirectory(final IPath absolutePath, final IPath relativePath, final IProgressMonitor monitor) throws CoreException;
 
 }
