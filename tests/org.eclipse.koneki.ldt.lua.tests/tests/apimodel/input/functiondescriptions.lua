eb12723f897a2764f8fc1c1da9b63355d1c32945
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2012-06-15 19:02:45 +0200

    Some API Model tests.

---
+++ tests/org.eclipse.koneki.ldt.lua.tests/tests/apimodel/input/functiondescriptions.lua
@@ -0,0 +1,65 @@
+---
+-- @module func
+
+---
+-- @function [parent = # func] empty
+
+---
+-- Short desc.
+-- @function [parent = # func] shortdescriptiondot
+
+---
+-- Short desc
+-- @function [parent = # func] shortdescriptionlineterminator
+
+---
+-- Short desc.
+-- Very long long long description.
+-- @function [parent = # func] longdescriptiondot
+
+---
+-- Short desc
+-- Very long long long description
+-- @function [parent = # func] longdescriptionlineterminator
+
+---
+-- @function [parent = # func] parameter
+-- @param p
+
+---
+-- @function [parent = # func] typed parameter
+-- @param #string p
+
+---
+-- @function [parent = # func] parameterwithdesription
+-- @param p Parameter description
+
+---
+-- @function [parent = # func] typedparameterwithdesription
+-- @param #number p Parameter description
+
+---
+-- @function [parent = # func] dotsasparameter
+-- @param ...
+-- @param #number p Parameter description
+
+---
+-- @function [parent = # func] dotsasreturn
+-- @return ...
+
+---
+-- @function [parent = # func] returnempty
+-- @return
+
+---
+-- @function [parent = # func] returnsinglevalue
+-- @return #number
+
+---
+-- @function [parent = # func] returnseveralvalues
+-- @return #nil, #string
+
+---
+-- @function [parent = # func] usagef
+-- @usage usagef()
+return nil
--- tests/org.eclipse.koneki.ldt.lua.tests/tests/apimodel/input/typedef.lua
+++ tests/org.eclipse.koneki.ldt.lua.tests/tests/apimodel/input/typedef.lua
@@ -1,3 +1,8 @@
+--
+-- This files stands for multiple type declaration tests as module declaration
+-- implicitely creates a type named after the module.
+--
+
 ---
 -- @module mod
 --
